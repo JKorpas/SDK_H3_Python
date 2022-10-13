13cd90dff086089721f5c492aa5de2a90c6f5b27
Jakub <korpasjakub@gmail.com>
2022-10-13 15:56:57 +0200

    Rotating creature queue

--- project/creature_queue.py
+++ project/creature_queue.py
@@ -1,12 +1,20 @@
 
 class CreatureQueue:
     def __init__(self, creature_list):
+        self.creatures = creature_list
         self.active_creature = None
-        self.creatures_queue = creature_list.copy()
+        self.creatures_queue = []
+        self.init_queue()
         self.next()
+        
+    def init_queue(self):
+        self.creatures_queue = self.creatures.copy()
 
     def get_active_creature(self):
         return self.active_creature
 
     def next(self):
+        #New Turn
+        if len(self.creatures_queue) == 0:
+            self.init_queue()
         self.active_creature = self.creatures_queue.pop(0)
--- project/creature_queue_test.py
+++ project/creature_queue_test.py
@@ -21,5 +21,18 @@ class TestCreatureQueue(unittest.TestCase):
         creature_queue.next()
         self.assertEqual(self.c, creature_queue.get_active_creature())
 
-    def test_for_shieee(self):
-        print(self.creature_list_test)
+    def test_if_queue_is_rotating(self):
+        creature_queue = CreatureQueue(self.creature_list_test)
+        self.assertEqual(self.a, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.b, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.c, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.a, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.b, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.c, creature_queue.get_active_creature())
+        creature_queue.next()
+        self.assertEqual(self.a, creature_queue.get_active_creature())
