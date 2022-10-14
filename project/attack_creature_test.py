296dcc067c0dd3b255f761bbbae4aaaafe868b0d
Jakub <korpasjakub@gmail.com>
2022-10-14 19:50:40 +0200

    attacking method and tests

--- project/attack_creature_test.py
+++ project/attack_creature_test.py
@@ -4,10 +4,10 @@ from creature import Creature
 class TestAttackCreature(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.attacker = Creature ("Attacker", 10, 2, 10, 5)
-        cls.defender = Creature ("Defender", 5, 5, 10, 5)
+        cls.attacker = Creature ("Attacker", 10, 2, 100, 5)
+        cls.defender = Creature ("Defender", 5, 5, 100, 5)
         
     def test_creature_should_lost_5hp_when_attacker_has_10_atk_and_defender_has_5_arm(self):
         self.attacker.attack(self.defender)
-        self.assertEqual(5, self.defender.get_current_hp())
+        self.assertEqual(95, self.defender.get_current_hp())
         
