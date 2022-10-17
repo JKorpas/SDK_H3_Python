6790a5572dd4a699ba1e851cff1cc6a5cab00063
Jakub <korpasjakub@gmail.com>
2022-10-17 18:49:57 +0200

    test with second attack but without counterattack

--- project/counterattack_creature_test.py
+++ project/counterattack_creature_test.py
@@ -8,3 +8,11 @@ class TestCounterAttack(unittest.TestCase):
         attacker.attack(defender)
         self.assertEqual(95, defender.get_current_hp())
         self.assertEqual(82, attacker.get_current_hp())
+        
+    def test_creature_should_not_make_second_counterattack(self):
+        attacker = Creature ("Attacker", 10, 2, 100, 5)
+        defender = Creature ("Defender", 20, 5, 100, 5)
+        attacker.attack(defender)
+        attacker.attack(defender)
+        self.assertEqual(90, defender.get_current_hp())
+        self.assertEqual(82, attacker.get_current_hp())
