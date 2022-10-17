9e659eebb745064b5a27884774f3772b80a4b1a2
Jakub <korpasjakub@gmail.com>
2022-10-17 18:52:46 +0200

    test with second attacker who not recive counter

--- project/counterattack_creature_test.py
+++ project/counterattack_creature_test.py
@@ -10,9 +10,11 @@ class TestCounterAttack(unittest.TestCase):
         self.assertEqual(82, attacker.get_current_hp())
         
     def test_creature_should_not_make_second_counterattack(self):
-        attacker = Creature ("Attacker", 10, 2, 100, 5)
+        attacker1 = Creature ("Attacker", 10, 2, 100, 5)
+        attacker2 = Creature ("Attacker", 10, 2, 100, 5)
         defender = Creature ("Defender", 20, 5, 100, 5)
-        attacker.attack(defender)
-        attacker.attack(defender)
+        attacker1.attack(defender)
+        attacker2.attack(defender)
         self.assertEqual(90, defender.get_current_hp())
-        self.assertEqual(82, attacker.get_current_hp())
+        self.assertEqual(82, attacker1.get_current_hp())
+        self.assertEqual(100, attacker2.get_current_hp())
